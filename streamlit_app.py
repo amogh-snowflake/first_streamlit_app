f713badaff55a2b765998e12df86de02c40c9244
amogh-snowflake <104901230+amogh-snowflake@users.noreply.github.com>
2022-05-04 17:55:23 +0530

    Update streamlit_app.py
    adding icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Bluberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard Biled Free Range EGG")
-streamlit.text("Avocado Toast")
+streamlit.text("🥣 Omega 3 & Bluberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard Biled Free Range EGG")
+streamlit.text("🥑🍞 Avocado Toast")
