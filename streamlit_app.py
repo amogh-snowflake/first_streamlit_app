1b310fd8bdb67f19d41ad19c3eaf07964a160f95
amogh-snowflake <104901230+amogh-snowflake@users.noreply.github.com>
2022-05-04 17:47:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
 streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header("Hello bro!! What's Coocking in your head")
+streamlit.text("Hola!! Martians.. what's for dinner?")
