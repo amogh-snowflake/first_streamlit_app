36ce2ed8a11a5a1f53feb19db4e9d79d939d13c0
amogh-snowflake <104901230+amogh-snowflake@users.noreply.github.com>
2022-05-04 17:54:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title("My Mom's New Healthy Dinner")
-streamlit.header("Hello bro!! What's Coocking in your head")
-streamlit.text("Hola!! Martians.. what's for dinner?")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Bluberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard Biled Free Range EGG")
+streamlit.text("Avocado Toast")
